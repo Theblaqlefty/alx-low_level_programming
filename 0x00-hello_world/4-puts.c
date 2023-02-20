001ef0b99b9044b95206e132cff07e048364b147
stephen <olorunlagbastephen@gmail.com>
2023-02-20 15:12:03 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main -A programthat prints a line with puts function
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
